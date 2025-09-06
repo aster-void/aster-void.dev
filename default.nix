10e7acd2f61e6b4d7ba79821d0441579a7b567ce
aster <137767097+aster-void@users.noreply.github.com>
2025-09-07 00:46:43 +0900

    package it with flake

---
+++ default.nix
@@ -0,0 +1,28 @@
+{
+  mkBunDerivation,
+  port,
+}: let
+  pname = "aster-void.dev";
+in
+  mkBunDerivation
+  {
+    src = ./.;
+    bunNix = ./nix/bun.nix;
+    packageJson = ./package.json;
+    buildPhase = ''
+      runHook preBuild
+      bun run build
+      runHook postBuild
+    '';
+    installPhase = ''
+      mkdir -p "$out"
+      rm -r "$out/build" "$out/node_modules" || true
+      cp build node_modules "$out" -r
+
+      install -Dm755 /dev/stdin "$out/bin/${pname}" <<EOF
+      #!/usr/bin/env bash
+      cd "$out/build"
+      PORT=${toString port} bun start
+      EOF
+    '';
+  }
