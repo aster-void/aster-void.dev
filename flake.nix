191e9bedce43afefa536d4f193bbd2e3033553ad
aster <137767097+aster-void@users.noreply.github.com>
2025-10-14 14:49:42 +0900

    meta/deploy: migrate to static adapter

--- flake.nix
+++ flake.nix
@@ -3,51 +3,13 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    bun2nix.url = "github:baileyluTCD/bun2nix";
-    bun2nix.inputs.nixpkgs.follows = "nixpkgs";
+    blueprint.url = "github:numtide/blueprint";
+    blueprint.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    bun2nix,
-    ...
-  }: (
-    flake-utils.lib.eachDefaultSystem (
-      system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-        website = pkgs.callPackage ./default.nix {
-          inherit (bun2nix.lib.${system}) mkBunDerivation;
-        };
-      in {
-        packages.default = website;
-        packages.www = website;
-        devShells.default = pkgs.mkShell {
-          packages = [
-            pkgs.bun
-            bun2nix.packages.${system}.default
-          ];
-        };
-        formatter = pkgs.alejandra;
-      }
-    )
-    // {
-      nixosModules.default = import ./nix/nixosModules/aster-void.dev.nix {inherit self;};
-      nixosModules."aster-void.dev" = import ./nix/nixosModules/aster-void.dev.nix {inherit self;};
-    }
-  );
-
-  nixConfig = {
-    extra-substituters = [
-      "https://cache.nixos.org"
-      "https://cache.garnix.io"
-    ];
-    extra-trusted-public-keys = [
-      # bun2nix
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-    ];
-  };
+  outputs = inputs:
+    inputs.blueprint {
+      inherit inputs;
+      prefix = "nix/";
+    };
 }
